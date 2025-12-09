1affcc0251b0190c8de99690e6e87d31ae3598b8
Piyush Choubey <piyushchoubay13@gmail.com>
2025-12-09 21:39:36 +0530

    Added String Comparison

---
+++ 3.Short Circuit and Chaining Comparison/StringComparison.py
@@ -0,0 +1,19 @@
+a = "HELLO"
+b = "hello"
+print(b > a)
+
+c = "ball"
+d = "APPLE"
+print(c > d)
+
+e = "12"
+f = "45"
+print(e < f)
+
+g = "apple"
+h = "Apple"
+print(g > h)
+
+e = " "
+f = "  "
+print(e < f)
